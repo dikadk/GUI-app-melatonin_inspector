9dd0c3adba1f5612509d64a6eacb94721f4a3493
dika <dimakiro2105@gmail.com>
2022-12-02 20:03:46 -0500

    Setup a "property editor" box#20 * editable x/y * editable width/height * is opaque * has a cached image * alpha * focus state * font * looknfeel

--- MainComponent.h
+++ MainComponent.h
@@ -4,36 +4,56 @@
 // directly. If you need to remain compatible with Projucer-generated builds, and
 // have called `juce_generate_juce_header(<thisTarget>)` in your CMakeLists.txt,
 // you could `#include <JuceHeader.h>` here instead, to make all your module headers visible.
-#include <juce_gui_extra/juce_gui_extra.h>
-#include "melatonin_inspector/melatonin_inspector.h"
 #include "PaddedComponent.h"
+#include "melatonin_inspector/melatonin_inspector.h"
+#include <juce_gui_extra/juce_gui_extra.h>
 
 //==============================================================================
-struct SampleLnF: juce::LookAndFeel_V4{
-    SampleLnF(){
-        setColour(juce::ResizableWindow::backgroundColourId, juce::Colours::crimson);
+struct SampleLnF : juce::LookAndFeel_V4
+{
+    SampleLnF()
+    {
+        setColour (juce::ResizableWindow::backgroundColourId, juce::Colours::crimson);
     }
 };
 
-struct PanelComp: juce::Component{
+namespace juce
+{
+    template <>
+    struct VariantConverter<Colour>
+    {
+        static Colour fromVar (const var& v) { return Colour ((uint32) static_cast<int> (v)); }
 
-    PanelComp(const juce::String& name): juce::Component(name){
-        setLookAndFeel(&lnf);
+        static var toVar (Colour v) { return { static_cast<int> (v.getARGB()) }; }
+    };
+}
+
+struct PanelComp : juce::Component
+{
+    PanelComp (const juce::String& name) : juce::Component (name)
+    {
+        setLookAndFeel (&lnf);
     }
 
-    ~PanelComp(){
-        setLookAndFeel(nullptr);
+    ~PanelComp()
+    {
+        setLookAndFeel (nullptr);
     }
 
-    void paint(juce::Graphics &g) override {
-        g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+    void paint (juce::Graphics& g) override
+    {
+        auto defColor = getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId);
+        auto bgColor = getProperties().getWithDefault ("Color", juce::VariantConverter<juce::Colour>::toVar (defColor));
+        g.fillAll (juce::VariantConverter<juce::Colour>::fromVar (bgColor));
     }
 
     SampleLnF lnf;
 };
 
-struct CustomComponent: juce::Component{
-    CustomComponent(const juce::String& name): juce::Component(name){
+struct CustomComponent : juce::Component
+{
+    CustomComponent (const juce::String& name) : juce::Component (name)
+    {
     }
 
     void paint(juce::Graphics &g) override {
@@ -41,26 +61,35 @@ struct CustomComponent: juce::Component{
     }
 };
 
-struct MultiplePanelComponent : juce::Component {
-    MultiplePanelComponent() {
-        auto compNames = {"Left","Center","Right"};
-        for(auto s: compNames){
-            auto c = new PanelComp(s);
+struct MultiplePanelComponent : juce::Component
+{
+    MultiplePanelComponent()
+    {
+        auto compNames = { "Left", "Center", "Right" };
+        for (auto s : compNames)
+        {
+            auto c = new PanelComp (s);
 
-            comps.add(c);
-            addAndMakeVisible(c);
+            comps.add (c);
+            addAndMakeVisible (c);
         }
+
+        comps[0]->getProperties().set ("Color", juce::VariantConverter<juce::Colour>::toVar (juce::Colours::darkolivegreen));
+        comps[0]->setOpaque(true);
+
+        comps[1]->setBufferedToImage(true);
     }
 
-    void resized() override {
+    void resized() override
+    {
         auto r = getLocalBounds();
 
-        //comps[0]->setBounds();
-        r.removeFromLeft(getWidth() / 3);
-        r.removeFromLeft(20);
-        comps[1]->setBounds(r.removeFromLeft(getWidth() / 3));
-        r.removeFromLeft(50);
-        comps[2]->setBounds(r);
+        r.removeFromLeft (getWidth() / 4);
+        comps[0]->setBounds (r.removeFromLeft (getWidth() / 8));
+        r.removeFromLeft (20);
+        comps[1]->setBounds (r.removeFromLeft (getWidth() / 3));
+        r.removeFromLeft (50);
+        comps[2]->setBounds (r);
     }
 
     juce::OwnedArray<juce::Component> comps;
@@ -70,24 +99,25 @@ struct MultiplePanelComponent : juce::Component {
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent : public juce::Component {
+class MainComponent : public juce::Component
+{
 public:
     //==============================================================================
     MainComponent();
 
     //==============================================================================
-    void paint(juce::Graphics &) override;
+    void paint (juce::Graphics&) override;
 
     void resized() override;
 
 private:
     //==============================================================================
     // Your private member variables go here...
-    melatonin::Inspector inspector{*this, false};
+    melatonin::Inspector inspector { *this, false };
 
     MultiplePanelComponent multiplePanelComponent;
-    juce::TextButton btn1{"Button1"};
-    juce::Slider slider1{"SliderOne"};
+    juce::TextButton btn1 { "Button1" };
+    juce::Slider slider1 { "SliderOne" };
 
     juce::OwnedArray<CustomComponent> components;
 
